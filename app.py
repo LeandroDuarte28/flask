1c546bbcd08f8425cb45469fcaa547df01effe2c
Leandro Duarte Silva <tecnologia.duarte@gmail.com>
2024-04-17 12:18:01 -0300

    listas e dicionario

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
+frutas = []
+registros = []
+
 #rota
-@app.route('/')
+@app.route('/', methods=["GET","POST"])
 def principal():
-    nome = "Leandro"
-    idade = 44
+
+    if request.method == "POST":
+        if request.form.get("fruta"):
+            frutas.append(request.form.get("fruta"))
     #reinderizando dentro do html
-    return render_template('index.html')
+    return render_template('index.html', frutas=frutas)
 
-@app.route('/sobre')
+@app.route('/sobre', methods=["GET", "POST"])
 def sobre():
-    return render_template('sobre.html')
+    if request.method == "POST":
+        if request.form.get("aluno") and request.form.get("nota"):
+            registros.append({"aluno": request.form.get("aluno"), "nota": request.form.get("nota")})
 
-
-# Acesso para o servidor......
-# http://127.0.0.1:5000/
+    return render_template('sobre.html', registros=registros)
